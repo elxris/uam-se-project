23de2f267b13d96798e0db5a205d59bbe4c418f0
jrzOmar <omar4jrz@gmail.com>
2019-06-25 13:27:21 -0500

    Creación de la vista navbar

---
+++ application/views/navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Sistema</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Respuestas <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Agregar Respuesta</a></li>
+          <li><a href="#">Modificar Respuesta</a></li>
+          <li><a href="#">Eliminar Respuesta</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Preguntas <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Agregar Preguntas</a></li>
+          <li><a href="#">Modificar Preguntas</a></li>
+          <li><a href="#">Eliminar Preguntas</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Cuestionarios <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Agregar Cuestionario</a></li>
+          <li><a href="#">Modificar Cuestionario</a></li>
+          <li><a href="#">Eliminar Cuestionario</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Encuestas <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Agregar Encuesta</a></li>
+          <li><a href="#">Modificar Encuesta</a></li>
+          <li><a href="#">Eliminar Encuesta</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
