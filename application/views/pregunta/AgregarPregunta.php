202ca7f360546b3d559c5d2557897bb766bba51e
Panterday <chavezzday@gmail.com>
2019-06-27 13:29:14 -0500

    Formato de vista para agregar una pregunta.

---
+++ application/views/pregunta/AgregarPregunta.php
@@ -0,0 +1,47 @@
+
+	<?= form_open("/pregunta/recibirDatos") ?>
+
+	<?php
+
+		$nombre = array(
+			'name' => 'Nombre' ,
+			'placeholder' => 'Ingresa pregunta', 
+                        'class' => 'form-control'
+                    );
+
+		$descripcion = array(
+			'name' => 'Desc' ,
+			'placeholder' => 'Ingresa una descripción',
+                        'class' => 'form-control'
+		);
+                
+                $botonGuardar = array(
+			'value' => 'Registrar',
+			'name' => '' ,
+			'class' => 'btn btn-primary'
+		);
+
+	?>
+        
+<div class="container">
+    <div class="col-4">
+        <h1>Ingresa pregunta</h1>
+        <br>
+        <br>
+        <?= form_label('Nombre: ', 'nombre') ?>
+        <?= form_input($nombre) ?>
+        </br>
+        </br>
+        <?= form_label('Descripción: ', 'descripcion') ?>
+        <?= form_textarea($descripcion) ?>
+        <br>
+        <br>
+
+        <?= form_submit($botonGuardar) ?>
+        <br>
+        <br>
+    </div>			
+
+</div>
+	
+	<?= form_close() ?>
